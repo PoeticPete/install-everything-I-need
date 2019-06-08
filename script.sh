0bf49357cffa658510a1d1de5cbf6d0e26b0cd78
Peter Tao <petertao95@gmail.com>
2019-06-08 16:57:41 -0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
+
 sudo apt install git -y
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EB3E94ADBE1229CF
 sudo add-apt-repository -y "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
@@ -8,3 +9,21 @@ wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt update
 sudo apt install code -y
+snap install postman
+sudo apt install snapd snapd-xdg-open
+
+# curl
+sudo apt install curl
+
+# pip3
+sudo apt-get update
+sudo apt-get -y install python3-pip
+
+# virtual envs
+sudo apt-get -y install python3-venv
+
+# aws cli
+pip3 install awscli --upgrade --user
+
+# vim
+sudo apt-get install vim -y
